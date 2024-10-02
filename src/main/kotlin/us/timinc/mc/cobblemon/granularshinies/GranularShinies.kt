4eddd6e5cb0d3802021b1bde96c4baca2f15393f
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-10-02 11:53:40 -0700

    added debugging

--- src/main/kotlin/us/timinc/mc/cobblemon/granularshinies/GranularShinies.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/granularshinies/GranularShinies.kt
@@ -2,18 +2,26 @@ package us.timinc.mc.cobblemon.granularshinies
 
 import com.cobblemon.mod.common.api.spawning.spawner.PlayerSpawnerFactory
 import net.fabricmc.api.ModInitializer
-import net.minecraft.util.Identifier
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
 import us.timinc.mc.cobblemon.granularshinies.config.ConfigBuilder
 import us.timinc.mc.cobblemon.granularshinies.config.MainConfig
 import us.timinc.mc.cobblemon.granularshinies.influences.ShinyOverride
+import java.util.*
 
 object GranularShinies : ModInitializer {
     @Suppress("MemberVisibilityCanBePrivate")
     const val MOD_ID = "granularshinies"
     lateinit var config: MainConfig
+    val logger = LogManager.getLogger(MOD_ID)
 
     override fun onInitialize() {
         config = ConfigBuilder.load(MainConfig::class.java, MOD_ID)
         PlayerSpawnerFactory.influenceBuilders.add(::ShinyOverride)
+    }
+
+    fun debug(msg: String, uuid: UUID? = null) {
+        if (!config.debug) return
+        logger.log(Level.INFO, if (uuid == null) msg else "$msg ($uuid)")
     }
 }
--- src/main/kotlin/us/timinc/mc/cobblemon/granularshinies/influences/ShinyOverride.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/granularshinies/influences/ShinyOverride.kt
@@ -6,17 +6,25 @@ import com.cobblemon.mod.common.api.spawning.detail.SpawnAction
 import com.cobblemon.mod.common.api.spawning.influence.SpawningInfluence
 import net.minecraft.server.network.ServerPlayerEntity
 import us.timinc.mc.cobblemon.granularshinies.GranularShinies.config
+import us.timinc.mc.cobblemon.granularshinies.GranularShinies.debug
+import java.util.*
 import kotlin.random.Random.Default.nextInt
 
 class ShinyOverride(@Suppress("unused") private val player: ServerPlayerEntity) : SpawningInfluence {
     override fun affectAction(action: SpawnAction<*>) {
         if (action !is PokemonSpawnAction) return
+        val uuid = UUID.randomUUID()
+        debug("A ${action.props.species} is spawning", uuid)
 
         val dummyMon = action.props.create()
         val found = config.overrides.entries.find { PokemonProperties.parse(it.key).matches(dummyMon) } ?: return
+        debug("Found matching shiny override of ${found.value}", uuid)
 
         val rate = found.value
-        if (nextInt(rate.toInt()) < 1) {
+        val roll = nextInt(rate.toInt())
+        debug("Rolled a $roll out of $rate (need to roll lower than 1)", uuid)
+        if (roll < 1) {
+            debug("Setting to shiny")
             action.props.shiny = true
         }
     }
